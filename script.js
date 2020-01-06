7687c2a9599010ce74485cd40170e5eb3d2225f9
tac0kat <bln0004@auburn.edu>
2020-01-06 13:47:51 +0700

    work pls

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('hey there!')
+
+
+function showPopup () { 
+	var popup = document.getElementById('popup');
+	popup.classList.add('show')
+};
+	
+
+
+function hidePopup () { 
+	var Popup = document.getElementById('popup');
+	popup.classList.remove('show')
+};
